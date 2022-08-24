7671b970626f143feeef68fd57bcd5bf600cfb36
ZzzDH <1056915818@qq.com>
2022-08-24 14:31:56 +0800

    Initial commit

---
+++ opinionmanage/src/main/java/com/group5/opinionmanage/controller/OpinionController.java
@@ -0,0 +1,13 @@
+package com.group5.opinionmanage.controller;
+
+/**
+ * @author 10569
+ * @version 1.0
+ * @description
+ * @Date 2022/8/24 14:17
+ */
+
+public class OpinionController {
+}
+
+
---
+++ opinionmanage/src/main/java/com/group5/opinionmanage/entity/Opinions.java
@@ -0,0 +1,13 @@
+package com.group5.opinionmanage.entity;
+
+/**
+ * @author 10569
+ * @version 1.0
+ * @description
+ * @Date 2022/8/24 14:17
+ */
+
+public class Opinions {
+}
+
+
