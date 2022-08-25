847bb77c83fb05353e6a90f251e107d3f3fd3a7c
m16a472 <89819248+m16a472@users.noreply.github.com>
2022-08-25 14:34:56 +0800

    11111111

--- spider/Excute.py
+++ spider/Excute.py
@@ -23,19 +23,16 @@ def question(url1, id, url2):
         return 0
     for data in jtext['data']:
         id = str(data.get('id'))
-        answer_count = data.get('answer_count')
-        data = {'id': id, 'answer_count': answer_count}
+        answercount = data.get('answercount')
+        data = {'id': id, 'answercount': answercount}
         datalist.append(data)
         pass
 
     return datalist
 
 
-def answer(url1, id, url2):
-    headers = {
-        'Accept': "application/json, text/plain, */*",
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
-    }
+def answer(url1, id, url2, head):
+    headers = {'Accept': "application/json, text/plain, */*", 'User-Agent': head}
     url = url1 + id + url2
     response = requests.get(url=url, headers=headers, timeout=300)
     sleep(0.5)
@@ -44,12 +41,19 @@ def answer(url1, id, url2):
     if len(jtext['data']) == 0:
         return 0
     for data in jtext['data']:
+        aid = str(data['id'])
         rawcontext = data['content']
-        voteup_count = data.get('voteup_count')
-        comment_count = data.get('comment_count')
+        if len(rawcontext) == 0:
+            return 0
+        voteupcount = data.get('voteup_count')
+        commentcount = data.get('comment_count')
+        heat = voteupcount + commentcount * 2
         questiontext = data['question'].get('title')
         context = pq(rawcontext).text()
-        data = {'context': context, 'voteup_count': voteup_count, 'comment_count': comment_count,'questiontext':questiontext}
+        qid = id
+        link = "https://www.zhihu.com/question/" + qid + "/answer/" + aid
+        data = {'aid': aid, 'context': context, 'voteupcount': voteupcount, 'commentcount': commentcount, 'heat': heat,
+                'questiontext': questiontext, 'qid': qid, 'link': link}
         contentList.append(data)
 
     return contentList
@@ -68,6 +72,26 @@ answerurl2 = '/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2C
              '.table_of_content.enabled&limit=5&offset=0' \
              '&platform=desktop&sort_by=default'
 questionList = [{'id': "527445211", 'answer_count': "605"}, {'id': "457368252", 'answer_count': "423"}]
+user_agent_list = [
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+    "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+    "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+]
 answerList = []
 for i in range(100):
     if i == len(questionList):
@@ -78,9 +102,12 @@ for i in range(100):
             if qitem not in questionList:
                 questionList.append(qitem)
     print(i)
+    j = 1
 for questionitem in questionList:
     end = int(questionitem.get('answer_count')) - int(questionitem.get('answer_count')) % 5
     for item in range(0, end, 5):
+        if j == 410:
+            continue
         answerurl2 = '/answers?include=data%5B%2A%5D.is_normal%2Cadmin_closed_comment%2Creward_info%2Cis_collapsed' \
                      '%2Cannotation_action%2Cannotation_detail%2Ccollapse_reason%2Cis_sticky%2Ccollapsed_by%2Csuggest_edit' \
                      '%2Ccomment_count%2Ccan_comment%2Ccontent%2Ceditable_content%2Cattachment%2Cvoteup_count' \
@@ -89,7 +116,7 @@ for questionitem in questionList:
                      '%2Cis_author%2Cvoting%2Cis_thanked%2Cis_nothelp%2Cis_recognized%3Bdata%5B%2A%5D.settings' \
                      '.table_of_content.enabled&limit=5&offset=' \
                      + str(item) + '&platform=desktop&sort_by=default'
-        alist = answer(answerurl1, questionitem.get('id'), answerurl2)
+        alist = answer(answerurl1, questionitem.get('id'), answerurl2, head=user_agent_list[item % 18])
         if alist != 0:
             for aitem in alist:
                 try:
@@ -97,7 +124,7 @@ for questionitem in questionList:
                     result = opidao.createcontent(aitem)
                     if result > 0:
                         print("写入成功")
+                        print(j)
+                        j += 1
                 finally:
                     opidao.close()
-
-
--- spider/dao/OpiDao.py
+++ spider/dao/OpiDao.py
@@ -9,8 +9,8 @@ class OpiDao(BaseDao):
         pass
 
     def createcontent(self, data={}):
-        sql = "insert into opinions (context,voteup_count,comment_count,questiontext) values (%s,%s,%s,%s)"
-        params = [data.get('context'), data.get('voteup_count'), data.get('comment_count'), data.get('questiontext')]
+        sql = "insert into opinions (aid,context,voteupcount,commentcount,heat,questiontext,qid,link) values (%s,%s,%s,%s,%s,%s,%s,%s)"
+        params = [data.get('aid'),data.get('context'), data.get('voteupcount'), data.get('commentcount'),data.get('heat'), data.get('questiontext'),data.get('qid'),data.get('link')]
         result = self.execute(sql, params)
         self.commit()
         return result
