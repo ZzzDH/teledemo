9ba5d52eaa9bd0e6aa1e58dd4165e1777a0a475e
ZzzDH <1056915818@qq.com>
2022-08-30 15:14:54 +0800

    fatal error

--- eurekaserver/src/main/java/com/group5/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/group5/eurekaserver/EurekaserverApplication.java
@@ -2,6 +2,8 @@ package com.group5.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
